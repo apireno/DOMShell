f941b6e890170a2d138b751cee842d57f4252448
apireno <sandro@pireno.com>
2026-03-22 16:47:16 -0400

    feat: enhance version command to display package version in domshell.js

--- mcp-server/bin/domshell.js
+++ mcp-server/bin/domshell.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-if (process.argv.includes("init")) {
+import { readFileSync } from "node:fs";
+
+if (process.argv.includes("--version") || process.argv.includes("-v")) {
+  const pkg = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf-8"));
+  console.log(pkg.version);
+} else if (process.argv.includes("init")) {
   import("../dist/init.js").then((m) => m.main());
 } else {
   import("../dist/index.js");
